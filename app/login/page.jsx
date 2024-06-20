b20b6697151962d7871f877a01fc0fa0f62900da
Tanarat Sudjai <149657689+TanaratSudjai@users.noreply.github.com>
2024-06-20 20:47:07 +0700

    update form

---
+++ app/login/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function login() {
+    
+  return (
+    <div className="mx-auto h-[100vh] w-full flex justify-center items-center bg-blue-300">
+      
+      <div className="form w-[500px] h-[500px] bg-white flex justify-center items-center rounded-md shadow-md">
+        FORM
+      </div>
+
+    </div>
+  )
+}
+
+export default login
