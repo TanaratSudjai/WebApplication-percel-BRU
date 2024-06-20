1914129bf2370421b26398405b62a2613f5b7819
Tanarat Sudjai <149657689+TanaratSudjai@users.noreply.github.com>
2024-06-20 21:02:42 +0700

    revert login

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function login() {
-    
   return (
-    <div className="mx-auto h-[100vh] w-full flex justify-center items-center bg-blue-300">
+    <div className="">
       
-      <div className="form w-[500px] h-[500px] bg-white flex justify-center items-center rounded-md shadow-md">
-        FORM
-      </div>
-
     </div>
-  )
+  );
 }
 
-export default login
+export default login;
