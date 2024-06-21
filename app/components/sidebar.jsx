fccb94228ccccb3f8ddb87c052118f426e8d5816
Tanarat Sudjai <149657689+TanaratSudjai@users.noreply.github.com>
2024-06-21 23:36:37 +0700

    master dashboard

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -1,12 +1,10 @@
+"use client"
+
 import React from "react";
 import {
-  Card,
-  Typography,
-  List,
   ListItem,
   ListItemPrefix,
   ListItemSuffix,
-  Chip,
 } from "@material-tailwind/react";
 
 import {
@@ -20,13 +18,13 @@ import {
 
 function sidebar() {
   return (
-    <div className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 border-2 rounded-xl">
+    <div className="h-[calc(90vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 border-2 rounded-xl">
 
       <div className="m-2 p-3">
         <h1 className="text-2xl font-bold">Sidebar</h1>
       </div>
 
-      <div className="mb-2 p-4 gap-4">
+      <div className="mb-2  p-4 gap-4">
         <ListItem className="hover:bg-slate-100 hover:text-slate-900 flex gap-2 text-1xl font-plain m-1 p-2">
           <ListItemPrefix>
             <PresentationChartBarIcon className="h-5 w-5" />
--- app/mock-table/page.jsx
+++ app/mock-table/page.jsx
@@ -25,185 +25,208 @@ function DashboardPage() {
   }, []);
 
   if (!dashboardData) {
-    return <div>Loading...</div>;
+    return (
+      <div className="mx-auto flex h-[100vh] justify-center items-center w-full text-2xl">
+        Loading...
+      </div>
+    );
   }
 
   return (
-    <div className="m-2 font-[sans-serif] w-full">
+    <div className="m-2 font-sans w-full">
       <div className="w-full">
         <Navbar />
       </div>
 
-      <div className="flex mt-3 w-[90%]">
-        <Sidebar />
+      <div className="flex gap-4 mt-3">
+        <div className="w-auto">
+          <Sidebar />
+        </div>
+        <div className="h-[90vh] overflow-y-auto w-full">
 
-        <div className="dashboard-container bg-black p-4 ">
-          <br />
-          <div className="">
-            <h1 className="text-2xl font-bold ml-2">Dashboard</h1>
-          </div>
-
-          {/* Staff Table */}
-          <div className="table-container m-2 h-[300px]">
-            <h2>Staff Table</h2>
-            <div className="relative h-full overflow-y-auto">
-              <table className="data-table min-w-full">
-                <thead className="bg-gray-100 sticky top-0">
-                  <tr>
-                    <th scope="col" className="px-2 py-2">
-                      ID
-                    </th>
-                    <th scope="col" className="px-2 py-2">
-                      Name
-                    </th>
-                    <th scope="col" className="px-1 py-2">
-                      Phone
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {dashboardData.staff.map((staff) => (
-                    <tr key={staff.staff_id}>
-                      <td className="px-2 py-2">{staff.staff_id}</td>
-                      <td className="px-2 py-2">{staff.staff_name}</td>
-                      <td className="px-1 py-2">{staff.staff_phone}</td>
+          <div className="grid grid-cols-2 gap-4">
+            {/* Staff Table */}
+            <div className="dashboard-container rounded-xl border-2 shadow-md p-4 bg-white">
+              <h2 className="text-lg font-semibold mb-4">Staff Table</h2>
+              <div className="table-container h-[150px] overflow-y-auto">
+                <table className="data-table min-w-full border-collapse">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th scope="col" className="px-4 py-2 border">
+                        ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Name
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Phone
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {dashboardData.staff.map((staff) => (
+                      <tr key={staff.staff_id} className="even:bg-gray-50">
+                        <td className="px-4 py-2 border">{staff.staff_id}</td>
+                        <td className="px-4 py-2 border">{staff.staff_name}</td>
+                        <td className="px-4 py-2 border">
+                          {staff.staff_phone}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            {/* Status Table */}
+            <div className="dashboard-container rounded-xl border-2 shadow-md p-4 bg-white">
+              <h2 className="text-lg font-semibold mb-4">Status Table</h2>
+              <div className="table-container overflow-y-auto">
+                <table className="data-table min-w-full border-collapse">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th scope="col" className="px-4 py-2 border">
+                        ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Name
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {dashboardData.status.map((status) => (
+                      <tr key={status.sta_id} className="even:bg-gray-50">
+                        <td className="px-4 py-2 border">{status.sta_id}</td>
+                        <td className="px-4 py-2 border">{status.sta_name}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            
+
+            {/* Parcels Table */}
+            <div className="dashboard-container rounded-xl border-2 shadow-md p-4 bg-white">
+              <h2 className="text-lg font-semibold mb-4">Parcels Table</h2>
+              <div className="table-container h-[360px] overflow-y-auto">
+                <table className="data-table min-w-full border-collapse">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th scope="col" className="px-4 py-2 border">
+                        ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Real ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Owner ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Staff ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Pickup Date
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Status ID
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {dashboardData.parcels.map((parcel) => (
+                      <tr key={parcel.par_id} className="even:bg-gray-50">
+                        <td className="px-4 py-2 border">{parcel.par_id}</td>
+                        <td className="px-4 py-2 border">
+                          {parcel.par_real_id}
+                        </td>
+                        <td className="px-4 py-2 border">{parcel.own_id}</td>
+                        <td className="px-4 py-2 border">{parcel.staff_id}</td>
+                        <td className="px-4 py-2 border">
+                          {parcel.pickupsdate}
+                        </td>
+                        <td className="px-4 py-2 border">{parcel.sta_id}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            {/* Delivers Table */}
+            <div className="dashboard-container rounded-xl border-2 shadow-md p-4 bg-white">
+              <h2 className="text-lg font-semibold mb-4">Delivers Table</h2>
+              <div className="table-container h-[360px] overflow-y-auto">
+                <table className="data-table min-w-full border-collapse">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th scope="col" className="px-4 py-2 border">
+                        DELIVERS ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        PARCEL ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        OWNER ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        DATE/TIME
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        NAME
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {dashboardData.delivers.map((delivered) => (
+                      <tr key={delivered.del_id} className="even:bg-gray-50">
+                        <td className="px-4 py-2 border">{delivered.del_id}</td>
+                        <td className="px-4 py-2 border">{delivered.par_id}</td>
+                        <td className="px-4 py-2 border">{delivered.own_id}</td>
+                        <td className="px-4 py-2 border">
+                          {delivered.deliverydate}
+                        </td>
+                        <td className="px-4 py-2 border">
+                          {delivered.deliver_name}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            {/* Owners Table */}
+            <div className="dashboard-container rounded-xl border-2 shadow-md p-4 bg-white">
+              <h2 className="text-lg font-semibold mb-4">Owners Table</h2>
+              <div className="table-container h-[150px] overflow-y-auto">
+                <table className="data-table min-w-full border-collapse">
+                  <thead className="bg-gray-100">
+                    <tr>
+                      <th scope="col" className="px-4 py-2 border">
+                        ID
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Name
+                      </th>
+                      <th scope="col" className="px-4 py-2 border">
+                        Phone
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {dashboardData.owners.map((owner) => (
+                      <tr key={owner.own_id} className="even:bg-gray-50">
+                        <td className="px-4 py-2 border">{owner.own_id}</td>
+                        <td className="px-4 py-2 border">{owner.own_name}</td>
+                        <td className="px-4 py-2 border">{owner.own_phone}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
-
-          {/* Owners Table */}
-          <div className="table-container">
-            <h2>Owners Table</h2>
-            <table className="data-table">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Name</th>
-                  <th>Phone</th>
-                </tr>
-              </thead>
-              <tbody>
-                {dashboardData.owners.map((owner) => (
-                  <tr key={owner.own_id}>
-                    <td>{owner.own_id}</td>
-                    <td>{owner.own_name}</td>
-                    <td>{owner.own_phone}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          {/* Status Table */}
-          <div className="table-container">
-            <h2>Status Table</h2>
-            <table className="data-table">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Name</th>
-                </tr>
-              </thead>
-              <tbody>
-                {dashboardData.status.map((status) => (
-                  <tr key={status.sta_id}>
-                    <td>{status.sta_id}</td>
-                    <td>{status.sta_name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          {/* Parcels Table */}
-          <div className="table-container">
-            <h2>Parcels Table</h2>
-            <table className="data-table">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Real ID</th>
-                  <th>Owner ID</th>
-                  <th>Staff ID</th>
-                  <th>Pickup Date</th>
-                  <th>Status ID</th>
-                </tr>
-              </thead>
-              <tbody>
-                {dashboardData.parcels.map((parcel) => (
-                  <tr key={parcel.par_id}>
-                    <td>{parcel.par_id}</td>
-                    <td>{parcel.par_real_id}</td>
-                    <td>{parcel.own_id}</td>
-                    <td>{parcel.staff_id}</td>
-                    <td>{parcel.pickupsdate}</td>
-                    <td>{parcel.sta_id}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          {/* delivers Table */}
-          <div className="table-container">
-            <h2>Delivers Table</h2>
-            <table className="data-table">
-              <thead>
-                <tr>
-                  <th>DELIVERS ID</th>
-                  <th>PARCEL ID</th>
-                  <th>OWNER ID</th>
-                  <th>DATA/TIME</th>
-                  <th>MAKE NAME</th>
-                </tr>
-              </thead>
-              <tbody>
-                {dashboardData.delivers.map((delivered) => (
-                  <tr key={delivered.del_id}>
-                    <td>{delivered.del_id}</td>
-                    <td>{delivered.par_id}</td>
-                    <td>{delivered.own_id}</td>
-                    <td>{delivered.deliverydate}</td>
-                    <td>{delivered.deliver_name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-
-          <style jsx>{`
-            .dashboard-container {
-              margin-left: 20px;
-              margin-top: 20px;
-              padding: 20px;
-              width: 100%;
-              border: 1px solid gray;
-            }
-            .table-container {
-              margin-bottom: 20px;
-            }
-            .data-table {
-              width: 100%;
-              border-collapse: collapse;
-              border: 1px solid #ddd;
-            }
-            .data-table th,
-            .data-table td {
-              padding: 8px;
-              text-align: left;
-              border: 1px solid #ddd;
-            }
-            .data-table th {
-              background-color: #f2f2f2;
-              font-weight: bold;
-            }
-            .data-table tr:nth-child(even) {
-              background-color: #f9f9f9;
-            }
-          `}</style>
         </div>
       </div>
     </div>
