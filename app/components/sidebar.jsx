673e0d1a915867aff451101282408416323e407e
ZonixEz <pnem2003@gmail.com>
2024-06-22 21:04:27 +0700

    not done yet

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import {
@@ -19,18 +19,19 @@ import {
 function sidebar() {
   return (
     <div className="h-[calc(90vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 border-2 rounded-xl">
-
       <div className="m-2 p-3">
         <h1 className="text-2xl font-bold">Sidebar</h1>
       </div>
 
-      <div className="mb-2  p-4 gap-4">
+      <div className="mb-2 p-4 gap-4">
+        <a href="../mock-table">
         <ListItem className="hover:bg-slate-100 hover:text-slate-900 flex gap-2 text-1xl font-plain m-1 p-2">
           <ListItemPrefix>
-            <PresentationChartBarIcon className="h-5 w-5" />
+            <PresentationChartBarIcon class="h-5 w-5" />
           </ListItemPrefix>
           Dashboard
         </ListItem>
+        </a>
 
         <ListItem className="hover:bg-slate-100 hover:text-slate-900 flex gap-2 text-1xl font-plain m-1 p-2">
           <ListItemPrefix>
@@ -44,17 +45,16 @@ function sidebar() {
             <InboxIcon className="h-5 w-5" />
           </ListItemPrefix>
           Inbox
-          <ListItemSuffix>
-            
-          </ListItemSuffix>
+          <ListItemSuffix></ListItemSuffix>
         </ListItem>
-
+        <a href="../owners">
         <ListItem className="hover:bg-slate-100 hover:text-slate-900 flex gap-2 text-1xl font-plain m-1 p-2">
           <ListItemPrefix>
             <UserCircleIcon className="h-5 w-5" />
           </ListItemPrefix>
-          Profile
+          Owers
         </ListItem>
+        </a>
 
         <ListItem className="hover:bg-slate-100 hover:text-slate-900 flex gap-2 text-1xl font-plain m-1 p-2">
           <ListItemPrefix>
