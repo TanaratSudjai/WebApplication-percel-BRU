2867f5dbbdc030313f61707bf5e987cbaf62594c
ZonixEz <pnem2003@gmail.com>
2024-06-22 18:39:58 +0700

    update

--- app/mock-table/page.jsx
+++ app/mock-table/page.jsx
@@ -130,8 +130,8 @@ function DashboardPage() {
                         <td className="px-4 py-2 border">
                           {parcel.par_real_id}
                         </td>
-                        <td className="px-4 py-2 border">{parcel.own_id}</td>
-                        <td className="px-4 py-2 border">{parcel.staff_id}</td>
+                        <td className="px-4 py-2 border">{parcel.own_name}</td>
+                        <td className="px-4 py-2 border">{parcel.staff_name}</td>
                         <td className="px-4 py-2 border">
                           {parcel.pickupsdate}
                         </td>
