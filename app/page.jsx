8af49203d210beff67cb145848303d947ac8a6f1
Tanarat Sudjai <149657689+TanaratSudjai@users.noreply.github.com>
2024-06-23 16:59:19 +0700

    add link to page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 function page() {
   return (
     <div className="h-[100vh] flex justify-center items-center w-full">
-      <div className="text-2xl">SingIn Successfully!</div>
+      <div className="text-2xl">Index ...</div>
+      
+      <div className="">
+        <Link href="/owners">call api</Link>
+      </div>
     </div>
   );
 }
