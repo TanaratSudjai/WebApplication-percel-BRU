93b01c5696eaa1f75040879389d2916767123eb7
Tanarat Sudjai <149657689+TanaratSudjai@users.noreply.github.com>
2024-06-23 23:27:57 +0700

    master auth

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,28 +1,48 @@
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function POST(req) {
+export async function GET() {
   try {
-    const { username, phone } = await req.json();
-    const hashedUsername = await bcrypt.hashSync(username, 10);
-    const newStaff = await prisma.staff.create({
-      data: {
-        staff_name: hashedUsername,
-        staff_phone: phone,
-      },
-    });
+    const account = await prisma.staff.findMany();
     return Response.json({
-      message: "Add admin into database for Username and Phone!",
-      data: {
-        newStaff,
-      },
+      account,
     });
   } catch (err) {
     return Response.json(
       {
-        err,
+        err: err.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function POST(req) {
+  try {
+    const { staff_name,staff_phone, email, password } = await req.json();
+    const hashedpassword = await bcrypt.hashSync(password, 10);
+    const accout = await prisma.staff.create({
+      data: {
+        staff_name,
+        staff_phone,
+        email,
+        password: hashedpassword,
+      },
+    });
+    return Response.json({
+      message: "Add accout into database",
+      data: {
+        accout,
+      },
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        error: error.message,
       },
       { status: 500 }
     );
--- app/api/staff/route.js
+++ app/api/staff/route.js
@@ -34,3 +34,34 @@ export async function POST(req) {
     return Response.json({ error }, { status: 500 });
   }
 }
+
+
+// import { Prisma, PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export async function POST(req) {
+//   try {
+//     const { username, phone } = await req.json();
+//     // const hashedUsername = await bcrypt.hashSync(username, 10);
+//     const newStaff = await prisma.staff.create({
+//       data: {
+//         staff_name: username,
+//         staff_phone: phone,
+//       },
+//     });
+//     return Response.json({
+//       message: "Add admin into database for Username and Phone!",
+//       data: {
+//         newStaff,
+//       },
+//     });
+//   } catch (err) {
+//     return Response.json(
+//       {
+//         err,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
