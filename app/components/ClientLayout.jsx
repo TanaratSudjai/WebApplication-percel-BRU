f789ebb9ea88a088f5626c33623c32cca7c5a745
Tanarat Sudjai <149657689+TanaratSudjai@users.noreply.github.com>
2024-06-24 01:35:03 +0700

    แก้ไข middleware [ ยังไม่เสร็จ ]

--- app/components/ClientLayout.jsx
+++ app/components/ClientLayout.jsx
@@ -1,28 +1,45 @@
 "use client";
+import React, { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 import { Inter } from "next/font/google";
-import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
-import SessionProvider from "./SessionProvider";
-import { isAuthPage } from "../utils/checkPath";//ตรวจสอบ routh ที่ต้องการแสดง nav และ sidebar
+import { isAuthPage } from "../utils/checkPath"; // ตรวจสอบ route ที่ต้องการแสดง nav และ sidebar
+
 const inter = Inter({ subsets: ["latin"] });
 
-export default function ClientLayout({ session, children }) {
+export default function ClientLayout({ children }) {
   const pathname = usePathname();
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  return (
-    <div className="font-sans w-[full]">
-      {isAuthPage(pathname) && <Navbar session={session} />} {/*ให้แสดง navbar ใน routh ที่เลือก*/ }
-      <div className="flex">
-        {isAuthPage(pathname) && ( 
-          <div>
-            <Sidebar session={session} /> {/*ให้แสดง sidebar ใน routh ที่เลือก*/ }
-          </div>
-        )}
-          <SessionProvider session={session}>{children}</SessionProvider>
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/");
+    }
+  }, [router, status]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "authenticated" && session.user) {
+    return (
+      <div className={`font-sans w-full ${inter.className}`}>
+        {isAuthPage(pathname) && <Navbar session={session} />}
+        <div className="flex">
+          {isAuthPage(pathname) && (
+            <div>
+              <Sidebar session={session} />
+            </div>
+          )}
+          {children}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 
+  return null; // or a fallback UI
 }
