a33293982b53f473a1ded83e8967b1755cf64865
ZonixEz <pnem2003@gmail.com>
2024-06-24 15:38:24 +0700

    clientlayout

--- app/components/ClientLayout.jsx
+++ app/components/ClientLayout.jsx
@@ -1,34 +1,21 @@
 "use client";
-import React, { useEffect } from "react";
-import { useRouter, usePathname } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 import { Inter } from "next/font/google";
+import { usePathname } from "next/navigation";
 import Navbar from "./navbar";
 import Sidebar from "./sidebar";
 import SessionProvider from "./SessionProvider";
 import { isAuthPage } from "../utils/checkPath"; //ตรวจสอบ routh ที่ต้องการแสดง nav และ sidebar
-const inter = Inter({ subsets: ["latin"] });
 
-export default function ClientLayout({ children }) {
+export default function ClientLayout({ session, children }) {
   const pathname = usePathname();
-  const { data: session, status } = useSession();
-  const router = useRouter();
 
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      router.push("/");
-    }
-  }, [router, status]);
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
   return (
     <div className="font-sans w-[full]">
       {" "}
       {/*ให้แสดง navbar ใน routh ที่เลือก*/}
       <div className="">
+
         <SessionProvider session={session}>
           <div>{isAuthPage(pathname) && <Navbar session={session} />}</div>
           <div className="flex">
@@ -42,6 +29,7 @@ export default function ClientLayout({ children }) {
           {children}
           </div>
         </SessionProvider>
+        
       </div>
     </div>
   );
