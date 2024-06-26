89abd74276243c2ae41a1324ae7bca4ea9b4054b
ZonixEz <pnem2003@gmail.com>
2024-06-26 09:10:19 +0700

    yoo

--- app/addparcel/page.jsx
+++ app/addparcel/page.jsx
@@ -95,19 +95,6 @@ function addparcel() {
                 className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
                 placeholder="แสกนหรือกรอกรหัสพัสดุ"
               />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#bbb"
-                stroke="#bbb"
-                className="w-4 h-4 absolute right-4"
-                viewBox="0 0 24 24"
-              >
-                <circle cx="10" cy="7" r="6" data-original="#000000"></circle>
-                <path
-                  d="M14 15H6a5 5 0 0 0-5 5 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 5 5 0 0 0-5-5zm8-4h-2.59l.3-.29a1 1 0 0 0-1.42-1.42l-2 2a1 1 0 0 0 0 1.42l2 2a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42l-.3-.29H22a1 1 0 0 0 0-2z"
-                  data-original="#000000"
-                ></path>
-              </svg>
             </div>
           </div>
 
