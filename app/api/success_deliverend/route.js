2d26dcee1049bc9f38bca2500773c113af09e0fa
TanaratSudjai <650112418012@gmail.com>
2024-08-03 15:33:05 +0700

    commit api success

--- app/api/success_deliverend/route.js
+++ app/api/success_deliverend/route.js
@@ -3,10 +3,14 @@ const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const unstatus = 1;
+    const unstatus = 2;
     const dataundeliverend = await prisma.parcel.findMany({
       include: {
-        Owner: true,
+        Owner:{
+          include: {
+            ownertype: true,
+          },
+        },
         Staff: true,
         Company: true,
         Status: true,
@@ -20,8 +24,10 @@ export async function GET() {
     const parcelsuccessdeliverend = dataundeliverend.map((parcel) => {
       return {
         ...parcel,
+        own_name: parcel.Owner && parcel.Owner.own_name,
         own_phone: parcel.Owner && parcel.Owner.own_phone,
         staff_name: parcel.Staff && parcel.Staff.staff_name,
+        ownertype_name: parcel.Owner?.ownertype?.ownertype_name,
         sta_name: parcel.Status && parcel.Status.sta_name,
         com_name: parcel.Company && parcel.Company.com_name,
         caategoryparcel_name:
