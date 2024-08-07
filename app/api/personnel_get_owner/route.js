438cfcae73e6877d238b84edf46f6bef0aae3a5e
TanaratSudjai <650112418012@gmail.com>
2024-08-07 19:48:53 +0700

    dev edit api

--- app/api/personnel_get_owner/route.js
+++ app/api/personnel_get_owner/route.js
@@ -2,24 +2,40 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export async function GET() {
   try {
-    const personnel = await prisma.owner.findMany({
+    const personnel = await prisma.parcel.findMany({
       where: {
-        ownertype_id: 2,
+        Owner: {
+          ownertype_id: 2,
+        },
       },
-      include:{
-        ownertype:true
-      }
+      include: {
+        Staff: true,
+        Company: true,
+        Status: true,
+        CategoryParcel: true,
+        Owner:{
+            include:{
+                ownertype:true
+            }
+        }
+      },
     });
 
-    return Response.json({ personnel });
+    const parcelstypeowner = personnel.map((parcel) => {
+      return {
+        par_name: parcel.par_name,
+        own_name: parcel.Owner?.own_name,
+        own_phone: parcel.Owner?.own_phone,
+        ownertype_name: parcel.Owner?.ownertype?.ownertype_name,
+        staff_name: parcel.Staff?.staff_name,
+        sta_name: parcel.Status?.sta_name,
+        com_name: parcel.Company?.com_name,
+        categoryparcel_name: parcel.CategoryParcel?.categoryparcel_name,
+      };
+    });
+
+    return Response.json({ personnel: parcelstypeowner });
   } catch (err) {
-    return Response.json(
-      {
-        message: "success get type owner",
-      },
-      {
-        status: 200,
-      }
-    );
+    return Response.json({ massage: err }, { status: 500 });
   }
 }
--- app/api/student_get_owner/route.js
+++ app/api/student_get_owner/route.js
@@ -2,24 +2,41 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export async function GET() {
   try {
-    const student = await prisma.owner.findMany({
+    const personnel = await prisma.parcel.findMany({
       where: {
-        ownertype_id: 1,
+        Owner: {
+          ownertype_id: 1,
+        },
       },
-      include:{
-        ownertype:true
-      }
+      include: {
+        Staff: true,
+        Company: true,
+        Status: true,
+        CategoryParcel: true,
+        Owner:{
+            include:{
+                ownertype:true
+            }
+        }
+      },
     });
 
-    return Response.json({ student });
+    //select แบบนี้ ดีสุด
+    const parcelstypeowner = personnel.map((parcel) => {
+      return {
+        par_name: parcel.par_name,
+        own_name: parcel.Owner?.own_name,
+        own_phone: parcel.Owner?.own_phone,
+        ownertype_name: parcel.Owner?.ownertype?.ownertype_name,
+        staff_name: parcel.Staff?.staff_name,
+        sta_name: parcel.Status?.sta_name,
+        com_name: parcel.Company?.com_name,
+        categoryparcel_name: parcel.CategoryParcel?.categoryparcel_name,
+      };
+    });
+
+    return Response.json({ personnel: parcelstypeowner });
   } catch (err) {
-    return Response.json(
-      {
-        message: "success get type owner",
-      },
-      {
-        status: 200,
-      }
-    );
+    return Response.json({ massage: err }, { status: 500 });
   }
 }
