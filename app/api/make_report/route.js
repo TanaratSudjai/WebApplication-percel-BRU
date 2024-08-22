26d54327cfcb604c9728aa1de43c24465f1ff119
TanaratSudjai <650112418012@gmail.com>
2024-08-22 09:19:37 +0700

    testing

--- app/api/make_report/route.js
+++ app/api/make_report/route.js
@@ -1,9 +1,15 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
     const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+
     const dataParcel = await prisma.parcel.findMany({
       include: {
         Owner: {
@@ -11,7 +17,6 @@ export async function GET() {
             ownertype: true,
           },
         },
-
         Staff: true,
         Company: true,
         Status: true,
@@ -20,29 +25,32 @@ export async function GET() {
       orderBy: {
         par_id: "desc",
       },
-      where:{
-        pickupsdate:today
-      }
+      where: {
+        pickupsdate: {
+          gte: today, 
+          lt: tomorrow, 
+        },
+      },
     });
 
     const parcelsWithPhone = dataParcel.map((parcel) => {
       return {
         ...parcel,
-        own_name: parcel.Owner && parcel.Owner.own_name,
-        own_phone: parcel.Owner && parcel.Owner.own_phone,
+        own_name: parcel.Owner?.own_name,
+        own_phone: parcel.Owner?.own_phone,
         ownertype_name: parcel.Owner?.ownertype?.ownertype_name,
-        staff_name: parcel.Staff && parcel.Staff.staff_name,
-        sta_name: parcel.Status && parcel.Status.sta_name,
-        com_name: parcel.Company && parcel.Company.com_name,
-        caategoryparcel_name:
-          parcel.CategoryParcel && parcel.CategoryParcel.categoryparcel_name,
+        staff_name: parcel.Staff?.staff_name,
+        sta_name: parcel.Status?.sta_name,
+        com_name: parcel.Company?.com_name,
+        categoryparcel_name: parcel.CategoryParcel?.categoryparcel_name,
       };
     });
 
-    //console.table(parcelsWithPhone);
+    console.table(parcelsWithPhone);
 
     return Response.json({ dataParcel: parcelsWithPhone }, { status: 200 });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    console.error(error);
+    return Response.json({ error: error.message }, { status: 500 });
   }
 }
